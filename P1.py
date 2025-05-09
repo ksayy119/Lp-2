9ee3bcc7eb8c256012a8bb69571c516304f49333
ksayy119 <sayyedkhudeja0@gmail.com>
2025-05-09 10:31:48 +0530

    Update P1.py

--- P1.py
+++ P1.py
@@ -3,14 +3,14 @@
 name = input("What's your name? ")
 print(f"Hello, {name}! Nice to meet you.")
 
-name = input("Enter your name: ")
+rollno = input("Enter your roll no.: ")
 age = input("Enter your age: ")
 student_class = input("Enter your class: ")
 mobile = input("Enter your mobile number: ")
 
 # Display the collected information
 print("\n--- Student Information ---")
-print("Name        :", name)
+print("Roll no        :", rollno)
 print("Age         :", age)
 print("Class       :", student_class)
 print("Mobile No.  :", mobile)
