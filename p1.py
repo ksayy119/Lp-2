9825314c54040c8df3ad740382a29da208eef303
ksayy119 <sayyedkhudeja0@gmail.com>
2025-05-09 09:58:49 +0530

    P1.py

--- p1.py
+++ p1.py
@@ -2,3 +2,15 @@
 
 name = input("What's your name? ")
 print(f"Hello, {name}! Nice to meet you.")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+student_class = input("Enter your class: ")
+mobile = input("Enter your mobile number: ")
+
+# Display the collected information
+print("\n--- Student Information ---")
+print("Name        :", name)
+print("Age         :", age)
+print("Class       :", student_class)
+print("Mobile No.  :", mobile)
